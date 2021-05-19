52a5cf06cc3b1c07c9c38b8004c50a1531d31f4e
Edward Villalobos <EdwardJr05@yahoo.com>
2021-05-18 21:18:30 -0500

    working on new navbar

--- csscript.js
+++ csscript.js
@@ -18,45 +18,12 @@ function showSlides() {
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-$(function () {
-  // script for full screen navbar
-  if ($(window).width() > 749) {
-    $(".navbar").find(".wrapperLink").show();
-    $(".navbar")
-      .find(".dropidown-button")
-      .mouseenter(function () {
-        if ($(".wrapperDropidown").hasClass("opened")) {
-          $(this).find(".dropidown-menu").css("display", "block");
-          $(this)
-            .siblings(".dropidown-button")
-            .find(".dropidown-menu")
-            .css("display", "none");
-        } else {
-          $(".wrapperDropidown").addClass("opened");
-          $(this).find(".dropidown-menu").slideDown("ease-out");
-        }
-      });
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const socialIcons = document.querySelector(".social-icons");
 
-    $(".navbar")
-      .find(".dropidown-button")
-      .hover(function () {
-        $(this).find(".navbar__item").toggleClass("borderBottom");
-      });
-
-    $(".wrapperDropidown").mouseleave(function () {
-      $(".dropidown-menu").stop(true, false).slideUp("ease-out");
-      $(".wrapperDropidown").removeClass("opened");
-    });
-  }
-
-  // script for small screen navbar
-  if ($(window).width() < 750) {
-    $(".navbar").find(".wrapperLink").hide();
-    $(".navbar")
-      .find(".wrapperBurger")
-      .click(function () {
-        $(this).next(".wrapperLink").slideToggle("ease-out");
-        $(this).toggleClass("animCross");
-      });
-  }
+navToggle.addEventListener("click", function () {
+  console.log(links.classList);
+  links.classList.toggle("show-links");
+  socialIcons.classList.toggle("show-icons");
 });
